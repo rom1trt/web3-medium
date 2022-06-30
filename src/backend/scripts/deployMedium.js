bb3de39414b20d00367aba8de7fab18ae657c8be
Romain TREFAULT <romain.trefault@epita.fr>
2022-06-30 10:07:54 +0200

    contract deployment

---
+++ src/backend/scripts/deployMedium.js
@@ -0,0 +1,60 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const { ethers, artifacts } = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Medium = await ethers.getContractFactory("Medium");
+  const medium = await Medium.deploy(
+    "Medium-Web3",
+    "Blog",
+    "1000000000000000000" // 1 MATIC
+);
+
+  await medium.deployed();
+
+  console.log("Medium deployed to:", medium.address);
+
+  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
+  saveFrontendFiles(medium, "Medium");
+}
+
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
