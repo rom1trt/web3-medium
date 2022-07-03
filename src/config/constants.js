ed05ce74ecbfcc92f35b9ef6ba26d7f890aea411
Romain TREFAULT <romain.trefault@epita.fr>
2022-07-03 11:36:40 +0200

    create list of blogs

---
+++ src/config/constants.js
@@ -0,0 +1,5 @@
+export const Url = "https://ipfs.io/ipfs";
+
+export const logoPost = "https://ipfs.io/ipfs/QmQA9U1xjCKAxfENc8NwGSvmcRkX5onceibQv7f15gb8fS?filename=decryptoLogo.png";
+
+export const sampleURI = "https://ipfs.io/ipfs/Qmd7DuscoYu3bqBavGxcxvoR1yZDhp8B4sNncyorZphucM"
